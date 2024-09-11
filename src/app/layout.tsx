901f3fd95c5d49ec2fef4c815ce1e2bf069cab0f
anurag.deol <anurag.deol@deskera.org>
2024-09-11 14:13:20 +0530

    implemented toggle theme between dark-light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import ThemeContextProvider from "@/context/theme-context";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
               {children}
               <Footer />
               <Toaster position="top-right" />
+              <ThemeSwitch />
             </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
